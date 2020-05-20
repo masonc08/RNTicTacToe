c7ec6a93e92d5c6ff6bca745ff1c32cae1ea85f2
Mason <mxmcao@gmail.com>
2020-05-19 21:00:34 -0400

    added initial modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import './App.css';
 
 const App = () => {
+  const [modal, setModal] = useState(true);
+  const toggle = () => setModal(!modal);
   return (
     <div className="App">
       <header className="App-header">
+        <link
+          rel='stylesheet'
+          href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css'
+        />
+        <Modal isOpen={modal} toggle={toggle}>
+          <ModalHeader toggle={toggle}>AITicTacToe</ModalHeader>
+          <ModalBody>
+            Welcome to AITicTacToe. Please select a play mode to begin.
+          </ModalBody>
+          <ModalFooter>
+            <div className="">
+              <Button color="primary" onClick={toggle}>One-Player</Button>{' '}
+              <Button color="primary" onClick={toggle}>Two-Player</Button>
+            </div>
+          </ModalFooter>
+        </Modal>
       </header>
     </div>
   );
