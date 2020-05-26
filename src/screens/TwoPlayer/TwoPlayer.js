fd7c5fa31507c151975e9c87734162eaf4a8b36f
Mason <mxmcao@gmail.com>
2020-05-25 21:23:38 -0400

    added logic

--- src/screens/TwoPlayer/TwoPlayer.js
+++ src/screens/TwoPlayer/TwoPlayer.js
@@ -29,6 +29,66 @@ export default () => {
     } else {
       return null;
     }
-  } 
-  return null;
+  }
+  const checkSum = sum => {
+    if(sum == -3){
+      alert('Player 1(X) wins!');
+      setGameState(false);
+      return true;
+    } else if (sum == 3){
+      alert('Player 2(O) wins!');
+      setGameState(false);
+      return true;
+    }
+    return false;
+  }
+  const checkWinner = (row, col) => {
+    //check if the column has a win
+    let sum = 0;
+    for (let i = 0; i < 3; i++) {
+      sum += board[i][col];
+    }
+    if (checkSum(sum)) return;
+    //check if the row has a win
+    sum = 0;
+    for (let i = 0; i < 3; i++) {
+      sum += board[row][i];
+    }
+    if (this.checkSum(sum)) return;
+    //check if the top left to bottom right diagonal has a win
+    sum = 0;
+    if (row === col) {
+      for (let i = 0; i < 3; i++) {
+        sum += board[i][i];
+      }
+      if (this.checkSum(sum)) return;
+      sum = 0;
+    }
+    //check if the top right to bottom left diagonal has a win
+    if (row == 2 && col == 0 || row == 0 && col == 2) {
+      for (let i = 0; i < 3; i++) {
+        sum += board[i][2-i];
+      }
+      if(this.checkSum(sum)) return;
+      sum = 0;
+    }
+    //check if it's a tie
+    if (this.state.turns == 9) {
+      alert("It was a tie!");
+      setGameState(false);
+      return;
+    }
+  }
+  const updateValue = (i, j) => {
+    // only respond if the tile isn't occupied, and if the game hasn't ended
+    if (board[i][j] == 0 && gameState) {
+      let newPosition = board;
+      newPosition[i][j] = playerTurn;
+      setBoard(newPosition);
+      setPlayerTurn((playerTurn == -1) ? 1 : -1);
+      setTurns(prevTurn => prevTurn + 1);
+      checkWinner(i, j);
+    }
+    return null;
+  }
 }
